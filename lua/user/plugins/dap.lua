b7989d35af30e4d89f9b6dff176427180a1650ae
crookedwarden <robjbrock71@gmail.com>
2022-07-03 02:24:53 -0500

    message

--- lua/user/plugins/dap.lua
+++ lua/user/plugins/dap.lua
@@ -2,7 +2,7 @@ return function()
   local dap = require "dap"
   dap.adapters.lldb = {
     type = "executable",
-    command = "/usr/bin/lldb-vscode",
+    command = "/usr/bin/lldb-vscode-14",
     name = "lldb",
   }
   dap.configurations.c = {
